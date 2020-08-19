44625049aa59dd670dcd6713a90d361314dce215
DwarfPorter <mrvlmor@gmail.com>
2020-08-19 22:10:06 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/team_2020_08_19/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.team_2020_08_19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
